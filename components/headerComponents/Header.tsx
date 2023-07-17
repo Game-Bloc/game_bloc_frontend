746a3ac6a2a4cac0f28f5df3c88ab098c4e63d8b
Emmanuel oluleye <91434033+Deonorla@users.noreply.github.com>
2023-07-17 19:16:47 +0100

    fixed sidebar]

--- components/headerComponents/Header.tsx
+++ components/headerComponents/Header.tsx
@@ -2,19 +2,28 @@ import React from "react";
 import { Container } from "../../styles/commonStyles/Container.styles";
 import { Text } from "../../styles/headerStyle/Header.styled";
 import Login from "./Login";
+import logo from "../../assets/images/gamelogo2.png";
 import Navbar from "./Navbar";
+import { Img } from "../../styles/commonStyles/Img";
 interface Props {
-  isSignedIn: any,
-  wallet: any,
-  gamebloc: any
+  isSignedIn: any;
+  wallet: any;
+  gamebloc: any;
 }
-const Header = ({isSignedIn, wallet, gamebloc}:Props) => {
+const Header = ({ isSignedIn, wallet, gamebloc }: Props) => {
   return (
-      <Container margin="2.75rem 6.25rem 1.5rem 6.25rem"  xmdmargin="1rem 1rem 1.5rem 1rem"  display="flex" flexDirection="row" justifyContent="space-between" alignItems="center" >
-        <Text fontsize="1.25rem" fontWeight={600} >Logo</Text>
-        <Navbar />
-        <Login isSignedIn={isSignedIn} wallet={wallet} gamebloc={gamebloc}/>
-      </Container>
+    <Container
+      margin="2.75rem 6.25rem 1.5rem 6.25rem"
+      xmdmargin="1rem 1rem 1.5rem 1rem"
+      display="flex"
+      flexDirection="row"
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <Img src={logo} alt="" width="22%" mdwidth="25%" smwidth="6.5rem" />
+      <Navbar />
+      <Login isSignedIn={isSignedIn} wallet={wallet} gamebloc={gamebloc} />
+    </Container>
   );
 };
 
